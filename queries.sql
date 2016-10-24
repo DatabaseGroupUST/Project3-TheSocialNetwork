6be044a1498ebc1e33732ae71d381f2022e11c80
Ahmed Alhonti <abonajd@gmail.com>
2016-10-23 22:48:49 -0500

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,23 @@
+--Q1
+select club_name from MEMBERSHIP where member_id =?;
+
+--Q2
+select club_name, count(member_id)
+from MEMBERSHIP
+join student on (member_id = person_id)
+where major = 'MATH'
+group by club_name;
+
+--Q3
+select club_name, count(member_id)
+from MEMBERSHIP
+where exists (select * from FRIENDS where person1 = ? and person2 = member_id and status = 'ACCEPTED')
+group by club_name;
+
+--Q4
+select count(like_id) as c, liker_id
+from LIKES
+where author_id = 0
+group by liker_id
+order by c DESC
+limit 3;
