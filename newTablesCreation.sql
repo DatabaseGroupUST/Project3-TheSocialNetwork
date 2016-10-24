1ad01657d080b3c4e593ad8f90bc6164c6f253da
Ahmed Alhonti <abonajd@gmail.com>
2016-10-23 21:13:11 -0500

    Update newTablesCreation.sql

--- newTablesCreation.sql
+++ newTablesCreation.sql
@@ -2,7 +2,7 @@ CREATE TABLE STUDENT_CLUBS
 (
     club_name VARCHAR(255),
     primary_contact VARCHAR(255),
-    mission_statement VARCHAR(5000)
+    mission_statement VARCHAR(5000),
     PRIMARY KEY (club_name)
 );
 
@@ -14,7 +14,7 @@ CREATE TABLE MEMBERSHIP
     end_date DATE NULL,
     PRIMARY KEY (club_name, member_id),
     FOREIGN KEY (club_name) REFERENCES STUDENT_CLUBS(club_name),
-    FOREIGN KEY (member_id) REFERENCES STUDENT(person_id),
+    FOREIGN KEY (member_id) REFERENCES student(person_id)
 );
 
 CREATE TABLE MESSAGE_BOARD
@@ -24,7 +24,7 @@ CREATE TABLE MESSAGE_BOARD
     club_name VARCHAR(255) NOT NULL,
     message TEXT NOT NULL,
     PRIMARY KEY (message_id),
-    FOREIGN KEY (club_name, author_id) REFERENCES membership(club_name, member_id)
+    FOREIGN KEY (club_name, author_id) REFERENCES MEMBERSHIP(club_name, member_id)
 );
 
 CREATE TABLE LIKES
@@ -34,7 +34,7 @@ CREATE TABLE LIKES
     liker_id INTEGER NOT NULL,
     club_name VARCHAR(255) NOT NULL,
     author_id INTEGER NOT NULL,
-    message_id INTEGER NOY NULL,
+    message_id INTEGER NOT NULL,
     PRIMARY KEY(like_id),
     FOREIGN KEY (club_name, liker_id) REFERENCES MEMBERSHIP(club_name, member_id)
 );
@@ -44,7 +44,7 @@ CREATE TABLE FRIENDS
     person1 INTEGER NOT NULL,
     person2 INTEGER NOT NULL,
     status ENUM('INVITED', 'ACCEPTED' , 'REJECTED') NOT NULL,
-    PRIMARY KEY (person1),
-    PRIMARY KEY (person2),
-    FOREIGN KEY (person1) REFERENCES STUDENT(person_id)
+    PRIMARY KEY (person1, person2),
+    FOREIGN KEY (person1) REFERENCES student(person_id),
+    FOREIGN KEY (person1) REFERENCES student(person_id)
 );
