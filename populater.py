1503f2e886f0e8bb923bfb85bfc9cb22b68a9782
NicholasFHayden <nicholasfhayden@gmail.com>
2016-10-23 22:17:19 -0500

    another table

--- populater.py
+++ populater.py
@@ -1,4 +1,18 @@
 import random
+import time
+
+def strTimeProp(start, end, format, prop):
+    stime = time.mktime(time.strptime(start, format))
+    etime = time.mktime(time.strptime(end, format))
+    ptime = stime + prop * (etime - stime)
+    return time.strftime(format, time.localtime(ptime))
+
+def randomDate(start, end, prop):
+    return strTimeProp(start, end, '%m/%d/%Y %I:%M %p', prop)
+
+clubs = []
+pc = []
+ms = []
 
 with open("./student_clubs_table.txt", 'w') as myFile:
     clubs = ["TriBeta",
@@ -8,7 +22,6 @@ with open("./student_clubs_table.txt", 'w') as myFile:
              "American Chemical Society",
              "Pre-health Professionals"]
 
-    pc = []
     for i in range(0,6):
         pc.append(random.randint(0,10000))
 
@@ -27,3 +40,15 @@ with open("./student_clubs_table.txt", 'w') as myFile:
         myFile.write('\t')
         myFile.write(ms[i])
         myFile.write('\n')
+
+with open("./membership_table.txt", 'w') as myFile:
+    for i in range(0,50):
+        myFile.write(clubs[random.randint(0,5)])
+        myFile.write('\t')
+        myFile.write(str(random.randint(0,10000)))
+        myFile.write('\t')
+        myFile.write(randomDate("1/1/2008 1:30 PM", "1/1/2009 4:50 AM", random.random()))
+        myFile.write('\t')
+        myFile.write(randomDate("1/1/2014 1:30 PM", "1/1/2015 4:50 AM", random.random()))
+        myFile.write('\n')
+
