b5b72846202063a8a6d16a1384cf33103d37f0ea
NicholasFHayden <nicholasfhayden@gmail.com>
2016-10-25 20:41:09 -0500

    another answer for 3

--- queries.sql
+++ queries.sql
@@ -1,23 +1,37 @@
---Q1
+-- Q1
 select club_name from MEMBERSHIP where member_id =?;
 
---Q2
+-- Q2
 select club_name, count(member_id)
 from MEMBERSHIP
 join student on (member_id = person_id)
 where major = 'MATH'
 group by club_name;
 
---Q3
+-- Q3
 select club_name, count(member_id)
 from MEMBERSHIP
 where exists (select * from FRIENDS where person1a = ? and person2b = ? and person2a = member_id and person2b and status = 'ACCEPTED')
 group by club_name;
 
---Q4
+-- Q4
 select count(like_id) as c, liker_id
 from LIKES
 where author_id = ?
 group by liker_id
 order by c DESC
 limit 3;
+
+-- Q3
+SELECT 
+    club_name, 
+    count(member_id)
+FROM
+    membership
+JOIN
+    friends
+ON
+    person1a = person2b AND
+    person1a = [target student_id] AND
+    status = 'ACCEPTED'
+group by club_name;
